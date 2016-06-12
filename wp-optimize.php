cd0abfb3394c238d1379c8a030b87a815c190b9d
Ruhani Rabin <plugins@ruhanirabin.com>
2016-06-12 16:01:51 +0800

    Version Update

--- wp-optimize.php
+++ wp-optimize.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Optimize
 Plugin URI: http://wp-optimize.ruhanirabin.com/
 Description: This plugin helps you to keep your database clean by removing post revisions and spam in a blaze. Additionally, it allows you to run the optimize command on your WordPress core tables (please use with caution).
-Version: 1.8.9.16
+Version: 1.9
 Author: Ruhani Rabin
 Author URI: https://github.com/ruhanirabin/WP-Optimize
 Text Domain: wp-optimize
@@ -11,7 +11,7 @@ Domain Path: /languages
 Git URI: https://github.com/ruhanirabin/wp-optimize
 GitHub Plugin URI: https://github.com/ruhanirabin/wp-optimize
 GitHub Branch: master
-    Copyright 2009-2014  Ruhani Rabin  (email : plugins@ruhanirabin.com)
+    Copyright 2009-2016  Ruhani Rabin  (email : plugins@ruhanirabin.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -45,7 +45,7 @@ global $current_user;
 //error_reporting( error_reporting() & ~E_NOTICE );
 
 if (! defined('WPO_VERSION'))
-    define('WPO_VERSION', '1.8.9.16');
+    define('WPO_VERSION', '1.9');
 
 if (! defined('WPO_PLUGIN_MAIN_PATH'))
   define('WPO_PLUGIN_MAIN_PATH', plugin_dir_path( __FILE__ ));
